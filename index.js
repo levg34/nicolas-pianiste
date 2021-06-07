9bc32e326196168313a2346c2f68ff4e6a4af93c
levg34 <guilhem.dross@laposte.net>
2021-06-07 21:41:22 +0200

    delete concert occurence

--- index.js
+++ index.js
@@ -534,6 +534,15 @@ app.delete('/admin/image/:id', (req, res) => {
     })
 })
 
+app.delete('/admin/concert/:id', (req, res) => {
+    const id = req.params.id
+
+    db.concerts.remove({ _id: id }, {}, function (err, numRemoved) {
+        if (err) res.status(500).json({err})
+        res.json({removed:numRemoved})
+    })
+})
+
 app.get('/admin/tokenvalid', (req, res) => {
     const user = {...req.user}
     const {exp} = user
