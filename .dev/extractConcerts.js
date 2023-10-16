5d7141b7dff81d7b1e80d6de386087d04e7fb266
levg34 <levg34@users.noreply.github.com>
2023-10-16 18:02:27 +0000

    repeat on li and not on ul

---
+++ .dev/extractConcerts.js
@@ -0,0 +1,11 @@
+$('span[ng-repeat^=concert]').each((index, element) => {
+    console.log(element)
+})
+
+$('#tour p').text()
+
+$('#tour p b').each((i,e) => console.log(e))
+
+$('#tour p b').map((i,e) => $(e).text())
+
+$('#tour ul.list-group').each((i,e) => console.log(e))
