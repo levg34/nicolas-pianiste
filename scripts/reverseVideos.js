a8a142edccaf522c7e81180e7c6a11f4adf50388
levg34 <levg34@users.noreply.github.com>
2023-10-16 18:02:27 +0000

    reverse videos except last

---
+++ scripts/reverseVideos.js
@@ -0,0 +1,17 @@
+const Datastore = require('nedb')
+const db = {}
+db.videos = new Datastore({ filename: 'data/videos', autoload: true })
+
+db.videos.count({},(err,count) => {
+    console.log(count)
+    db.videos.find({}).sort({index: 1}).exec(function (err, docs) {
+        if (err) console.error(err)
+        docs.filter(video => video.index !== count).forEach(video => {
+            console.log(video.index+' ->',(count+1) - video.index)
+            db.videos.update({_id: video._id},{$set:{index: (count+1) - video.index}},{},(err, updated) => {
+                if (err) console.error(err)
+                console.log('updated: '+updated)
+            })
+        })
+    })
+})
