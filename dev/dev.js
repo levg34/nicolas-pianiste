f1a44de44719ac4072fdb48e3024d3021d66e8e6
levg34 <levg34@users.noreply.github.com>
2023-10-16 18:14:13 +0000

    refactor with angularjs: bio + studies

--- dev/dev.js
+++ dev/dev.js
@@ -1,3 +1,9 @@
 $("#links a").each(function() {console.log('{name:\''+$(this).text()+'\',url:\''+$(this).attr('href')+'\'}')})
 
 $("#myCarousel div.item").each(function() {console.log('{title:\''+$(this).find('h3').text()+'\',url:\''+$(this).find('img').attr('src')+'\',description:\''+$(this).find('p').text()+'\'}')})
+
+var table = ''
+$("#studies p").each(function() {
+	table += "'"+$(this).text()+"',\n"
+})
+console.log(table)
