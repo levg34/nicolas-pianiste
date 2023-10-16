c22d847d0cf9a18df6fec552f6bfabf58d8a1824
levg34 <levg34@users.noreply.github.com>
2023-10-16 18:02:24 +0000

    add restore scripts for bio and studies

---
+++ scripts/restoreStudies.js
@@ -0,0 +1,39 @@
+const Datastore = require('nedb')
+const db = {}
+db.studies = new Datastore({ filename: 'data/studies', autoload: true })
+
+db.studies.remove({}, { multi: true }, function (err, numRemoved) {
+    if (err) console.error(err)
+    console.log({numRemoved})
+})
+
+const studiesData = require('../backup/studies.json')
+
+const {title,awardsTitle,paragraphs,awards} = studiesData
+
+insertInDB({title, awardsTitle})
+
+let index = 0
+paragraphs.forEach(data => {
+    insertInDB({
+        paragraph: data,
+        index
+    })
+    ++index
+})
+
+awards.forEach(data => {
+    insertInDB({
+        award: data,
+        index
+    })
+    ++index
+})
+
+function insertInDB(data) {
+    db.studies.insert(data, function (err, newDoc) {
+        if (err)
+            console.error(err)
+        console.log(newDoc)
+    })
+}
