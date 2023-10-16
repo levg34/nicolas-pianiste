c0a2a3473abf4ae50e1b46e0e57290495e2934d0
levg34 <levg34@users.noreply.github.com>
2023-10-16 18:13:20 +0000

    reverse videos

--- scripts/reverseVideos.js
+++ scripts/reverseVideos.js
@@ -6,7 +6,7 @@ db.videos.count({},(err,count) => {
     console.log(count)
     db.videos.find({}).sort({index: 1}).exec(function (err, docs) {
         if (err) console.error(err)
-        docs.filter(video => video.index !== count).forEach(video => {
+        docs.forEach(video => {
             console.log(video.index+' ->',(count+1) - video.index)
             db.videos.update({_id: video._id},{$set:{index: (count+1) - video.index}},{},(err, updated) => {
                 if (err) console.error(err)
