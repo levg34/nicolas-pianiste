1dd6554ee67ad9732f65fe187cb71a124ebd3b0c
levg34 <levg34@users.noreply.github.com>
2023-10-16 18:02:25 +0000

    insert repertory data into database

---
+++ scripts/restoreRepertory.js
@@ -0,0 +1,31 @@
+const Datastore = require('nedb')
+const db = {}
+db.repertory = new Datastore({ filename: 'data/repertory', autoload: true })
+
+db.repertory.remove({}, { multi: true }, function (err, numRemoved) {
+    if (err) console.error(err)
+    console.log({numRemoved})
+})
+
+const repertoryData = require('../backup/repertory.json')
+
+repertoryData.forEach(data => {
+    const title = data.title
+
+    data.items.forEach(item => {
+        const subtitle = item.title
+        item.list.forEach(e => {
+            const repertoryElement = {
+                title,
+                content: e
+            }
+            if (subtitle) {
+                repertoryElement.subtitle = subtitle
+            }
+            db.repertory.insert(repertoryElement, function (err, newDoc) {
+                if (err) console.error(err)
+                console.log(newDoc)
+            })
+        })
+    })
+})
