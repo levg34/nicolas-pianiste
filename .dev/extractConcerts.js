2a738d7a77b24b93bf5634a905a970d3764d7613
levg34 <levg34@users.noreply.github.com>
2023-10-16 18:02:27 +0000

    extract data parts for concerts

--- .dev/extractConcerts.js
+++ .dev/extractConcerts.js
@@ -9,3 +9,6 @@ $('#tour p b').each((i,e) => console.log(e))
 $('#tour p b').map((i,e) => $(e).text())
 
 $('#tour ul.list-group').each((i,e) => console.log(e))
+$('#tour ul:not(.list-group)').each((i,e) => console.log(e))
+
+$('#tour span[style]').each((i,e) => console.log(e))
