607b494be959a2fb90119027b4f14c5beff3e2fd
levg34 <levg34@users.noreply.github.com>
2023-11-04 11:54:41 +0000

    add solid router

--- pages/src/App.tsx
+++ pages/src/App.tsx
@@ -1,10 +1,14 @@
+import { Route, Router, Routes } from '@solidjs/router'
 import { Component } from 'solid-js'
 import Page from './Page'
 
 const App: Component = () => {
-    return <>
-        <Page headerImageUrl="https://nicolasdross.fr/uploads/15d0b22a39ff686e73991ef52d5c9d76" pageName="Duo d'Oro" />
-    </>
+    return <Router>
+        <Routes>
+            <Route path="/" element={<div>Nothing to see here</div>}/>
+            <Route path="/pages/:pageId" element={<Page headerImageUrl="https://nicolasdross.fr/uploads/15d0b22a39ff686e73991ef52d5c9d76" pageName="Duo d'Oro" />}/>
+        </Routes>
+    </Router>
 }
 
 export default App
