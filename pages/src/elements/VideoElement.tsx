882c05622438a61a007cfbb4de9e67f7d193c35b
levg34 <levg34@users.noreply.github.com>
2023-11-12 17:47:24 +0000

    does not work

--- pages/src/elements/VideoElement.tsx
+++ pages/src/elements/VideoElement.tsx
@@ -1,13 +1,34 @@
+import { SolidPlyr } from 'solid-plyr'
+// import 'solid-plyr/dist/esm/index.css.map'
+
 type Props = {
     url: string
     thumbUrl: string
 }
 
 export default (props: Props) => (
-    <a href={props.url} target="_blank">
-        <div style="position: relative; width: 100%">
-            <img style="width: 100%" src={props.thumbUrl} />
-            <span style="position: absolute; left: 50%; top: 50%; transform: translate(-50%, -50%); z-index: 1">▶</span>
-        </div>
-    </a>
+    // <div style="position: relative; width: 100%">
+    <SolidPlyr
+        source={{
+            type: 'video',
+            // sources: [
+            //     {
+            //         src: props.url
+            //     }
+            // ]
+            sources: [
+                {
+                    src: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-720p.mp4',
+                    type: 'video/mp4',
+                    size: 720
+                },
+                {
+                    src: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-1080p.mp4',
+                    type: 'video/mp4',
+                    size: 1080
+                }
+            ]
+        }}
+    />
+    // </div>
 )
