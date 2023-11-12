44252d32aa0788f5741e7c5f9458a33e7b6dbb03
levg34 <levg34@users.noreply.github.com>
2023-11-12 17:05:11 +0000

    add play button on video

--- pages/src/elements/VideoElement.tsx
+++ pages/src/elements/VideoElement.tsx
@@ -5,6 +5,9 @@ type Props = {
 
 export default (props: Props) => (
     <a href={props.url} target="_blank">
-        <img style="width: 100%" src={props.thumbUrl} />
+        <div style="position: relative; width: 100%">
+            <img style="width: 100%" src={props.thumbUrl} />
+            <span style="position: absolute; left: 50%; top: 50%; transform: translate(-50%, -50%); z-index: 1">▶</span>
+        </div>
     </a>
 )
