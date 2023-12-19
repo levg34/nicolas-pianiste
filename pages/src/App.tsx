a123166087977bb6e11811267c8ad1b39d708a90
levg34 <levg34@users.noreply.github.com>
2023-12-19 15:01:27 +0000

    default route caught

--- pages/src/App.tsx
+++ pages/src/App.tsx
@@ -5,8 +5,8 @@ import Page from './Page'
 const App: Component = () => {
     return <Router>
         <Routes>
-            <Route path="/" element={<div>Nothing to see here</div>}/>
             <Route path="/pages/:pageId" element={<Page />}/>
+            <Route path="*" element={<div>Nothing to see here</div>}/>
         </Routes>
     </Router>
 }
